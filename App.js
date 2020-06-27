df6547be4cba78fe4563a8153750239c1752b44b
KogaYamada <koga0087y@gmail.com>
2020-06-27 21:57:03 +0900

    initial commit

--- App.js
+++ App.js
@@ -1,21 +1,21 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
+import SignupScreen from './src/screens/SignupScreen';
+import SigninScreen from './src/screens/SigninScreen';
 
-export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+const navigator = createStackNavigator(
+  {
+    Signup: SignupScreen,
+    Signin: SigninScreen,
   },
-});
+  {
+    initialRouteName: 'Signup',
+    defaultNavigationOptions: {
+      title: 'Connpass App',
+    },
+  }
+);
+
+export default createAppContainer(navigator);
