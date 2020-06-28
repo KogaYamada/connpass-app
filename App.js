be96223c5aa7e7a72deca2403d5271b5637c02f2
Daichi0914 <goludrozya44@gmail.com>
2020-06-29 01:29:37 +0900

    MyPageの編集とXDのテスト

--- App.js
+++ App.js
@@ -4,14 +4,16 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignupScreen from './src/screens/SignupScreen';
 import SigninScreen from './src/screens/SigninScreen';
+import MyPageScreen from './src/screens/MyPageScreen';
 
 const navigator = createStackNavigator(
   {
     Signup: SignupScreen,
     Signin: SigninScreen,
+    MyPage: MyPageScreen,
   },
   {
-    initialRouteName: 'Signup',
+    initialRouteName: 'MyPage',
     defaultNavigationOptions: {
       title: 'Connpass App',
     },
--- src/screens/MyPageScreen.jsx
+++ src/screens/MyPageScreen.jsx
@@ -1,12 +1,33 @@
 import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import { View, Text, StyleSheet } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+import { Avatar } from 'react-native-elements';
 
 const MyPageScreen = () => {
   return (
-    <View></View>
-  )
-}
+    <View>
+      <View style={styles.avatarView}>
+        <Avatar
+          rounded
+          icon={{ name: 'user', type: 'font-awesome' }}
+          overlayContainerStyle={{backgroundColor: 'pink'}}
+          size="xlarge"
+        />
+        <Text style={styles.avatarName}>User Name</Text>
+      </View>
+      <TouchableOpacity>
+        <Text>プロフィール設定</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>設定</Text>
+      </TouchableOpacity>
+      <TouchableOpacity>
+        <Text>ログアウト</Text>
+      </TouchableOpacity>
+    </View>
+  );
+};
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({});
 
-export default MyPageScreen
+export default MyPageScreen;
