7b71616ab8a7ab99c3f8cfb6885aadac755ccf2f
oka! <66175301+okamoto-tosei@users.noreply.github.com>
2020-06-28 15:54:27 +0900

    My_Pageコンポーネントの作成

---
+++ components/My_Page.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+
+const My_Page = () => {
+    return(
+        <View>
+            <Text>My_Page</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({});
+
+export default My_Page;
