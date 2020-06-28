21e964875204d0b63af59bf8a9ed1eee12c9c5a3
KogaYamada <koga0087y@gmail.com>
2020-06-29 01:10:44 +0900

    メッセージの見た目作成
    メッセージのリストを作成。
    メッセージの内容は未実装。

--- App.js
+++ App.js
@@ -4,11 +4,15 @@ import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
 import SignupScreen from './src/screens/SignupScreen';
 import SigninScreen from './src/screens/SigninScreen';
+import MessageListScreen from './src/screens/MessageListScreen';
+import MessageDetailScreen from './src/screens/MessageDetailScreen';
 
 const navigator = createStackNavigator(
   {
     Signup: SignupScreen,
     Signin: SigninScreen,
+    MessageList: MessageListScreen,
+    MessageDetail: MessageDetailScreen,
   },
   {
     initialRouteName: 'Signup',
