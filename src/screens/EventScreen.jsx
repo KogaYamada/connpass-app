5cf1f21d32cea620a097c218a4225b21a05aec34
oka! <66175301+okamoto-tosei@users.noreply.github.com>
2020-06-28 18:33:34 +0900

    イベントスクリーンの作成

---
+++ src/screens/EventScreen.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import {View, StyleSheet, Text} from 'react-native';
+
+const EventScreen = () => {
+    return(
+        <Text>Event Screen</Text>
+    );
+};
+
+const styles = StyleSheet.create({
+
+});
+
+export default EventScreen;
