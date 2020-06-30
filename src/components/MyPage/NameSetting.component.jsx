9168ff8905dc012b338112f56aaa0af8a76a06f7
Daichi0914 <goludrozya44@gmail.com>
2020-06-30 16:33:08 +0900

    yarn add react-native-keyboard-aware-scroll-view を実行しました。

--- src/components/MyPage/NameSetting.component.jsx
+++ src/components/MyPage/NameSetting.component.jsx
@@ -1,29 +1,54 @@
 import React from 'react';
-import { View, Text, StyleSheet, TextInput, Label } from 'react-native';
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  Label,
+  ScrollView,
+  KeyboardAvoidingView,
+} from 'react-native';
 
 import { Input, Icon } from 'react-native-elements';
 import useInput from '../../hooks/useInput';
 
 const NameSetting = () => {
   return (
-    <View style={styles.inputContainer}>
-      <Input
-        style={styles.userName}
-        label="ニックネーム"
-        leftIcon={{ name: 'user-edit', type: 'font-awesome-5', size: 20 }}
-      />
-      <Input
-        style={styles.userUrl}
-        label="URLなど"
-        leftIcon={{ name: 'link', type: 'font-awesome-5', size: 20 }}
-        // leftIconContainerStyle={{ size: 20 }}
-      />
-      <Input
-        style={styles.userIntroduction}
-        label="自己紹介"
-        leftIcon={{ name: 'profile', type: 'antdesign', size: 20 }}
-      />
-    </View>
+    <ScrollView style={styles.inputContainer}>
+      <KeyboardAvoidingView behavior="padding">
+        <Input
+          style={styles.userName}
+          label="ニックネーム"
+          leftIcon={{
+            name: 'user-edit',
+            type: 'font-awesome-5',
+            size: 20,
+            marginRight: 15,
+          }}
+        />
+        <Input
+          style={styles.userUrl}
+          label="URLなど"
+          leftIcon={{
+            name: 'link-variant',
+            type: 'material-community',
+            size: 20,
+            marginRight: 15,
+          }}
+        />
+        <Input
+          style={styles.userIntroduction}
+          label="自己紹介"
+          leftIcon={{
+            name: 'profile',
+            type: 'antdesign',
+            size: 20,
+            marginRight: 15,
+            paddingLeft: 3,
+          }}
+        />
+      </KeyboardAvoidingView>
+    </ScrollView>
   );
 };
 
@@ -36,9 +61,9 @@ const styles = StyleSheet.create({
     borderWidth: 1,
     borderColor: '#ddd',
   },
-  userName: {},
+  userName: {flex: 1},
   userUrl: {},
-  userIntroduction: {},
+  userIntroduction: {flex: 1},
 });
 
 export default NameSetting;
