1daf1f3d829521a48f0e0087177cfb8c9d941c61
KogaYamada <koga0087y@gmail.com>
2020-07-07 13:30:26 +0900

    firebase の追加
    firebaseのapi keyを取得
    auth,firestore,storageが使用可能

---
+++ src/api/firebase/firebase.js
@@ -0,0 +1,19 @@
+import * as firebase from 'firebase';
+import 'firebase/auth';
+import 'firebase/firestore';
+import 'firebase/storage';
+
+var firebaseConfig = {
+  apiKey: 'AIzaSyDT2RoxJOR79JcQ9sOqiLKdCUAMmDKSsNA',
+  authDomain: 'connpass-app-cbe80.firebaseapp.com',
+  databaseURL: 'https://connpass-app-cbe80.firebaseio.com',
+  projectId: 'connpass-app-cbe80',
+  storageBucket: 'connpass-app-cbe80.appspot.com',
+  messagingSenderId: '24734706535',
+  appId: '1:24734706535:web:89830de317c37438a89dc2',
+  measurementId: 'G-2P840P6RBL',
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
