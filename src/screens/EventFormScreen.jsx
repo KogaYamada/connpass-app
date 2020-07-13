fecf66459a73cdbc762ad56b310c45e2a57914be
KogaYamada <koga0087y@gmail.com>
2020-07-13 22:23:52 +0900

    .prettierrcの変更、間違っていたパスの修正

--- src/screens/EventFormScreen.jsx
+++ src/screens/EventFormScreen.jsx
@@ -1,100 +1,117 @@
 import React, { Component, useState } from 'react';
-import { Container, Header, Tab, Tabs, TabHeading, Icon, Text,Button } from 'native-base';
+import {
+  Container,
+  Header,
+  Tab,
+  Tabs,
+  TabHeading,
+  Icon,
+  Text,
+  Button,
+} from 'native-base';
 import MaterialTabs from 'react-native-material-tabs';
-import {View, StyleSheet,ScrollView,SafeAreaView } from 'react-native';
-import SearchBar from './SearchBar';
+import { View, StyleSheet, ScrollView, SafeAreaView } from 'react-native';
+import SearchBar from '../components/SearchBar';
 import EventComponent from '../components/EventComponent';
 
-
-const EventFormScreen = ({navigation}) => {
-  
-
-    return (
-      <>
+const EventFormScreen = ({ navigation }) => {
+  return (
+    <>
       <SearchBar />
-        <Container>
+      <Container>
         <Tabs style={styles.textTab}>
-          <Tab activeTabStyle={{backgroundColor: 'red'}} heading={ <TabHeading><Text style={styles.textTab}>フロントエンド</Text></TabHeading>}>
-          <View style={styles.viewWrap}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          </ScrollView>
-          </View>
+          <Tab
+            activeTabStyle={{ backgroundColor: 'red' }}
+            heading={
+              <TabHeading>
+                <Text style={styles.textTab}>フロントエンド</Text>
+              </TabHeading>
+            }>
+            <View style={styles.viewWrap}>
+              <ScrollView showsVerticalScrollIndicator={false}>
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+              </ScrollView>
+            </View>
           </Tab>
-          <Tab heading={ <TabHeading><Text style={styles.textTab}>バックエンド</Text></TabHeading>}>
-          <View style={styles.viewWrap}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          </ScrollView>
-          </View>
+          <Tab
+            heading={
+              <TabHeading>
+                <Text style={styles.textTab}>バックエンド</Text>
+              </TabHeading>
+            }>
+            <View style={styles.viewWrap}>
+              <ScrollView showsVerticalScrollIndicator={false}>
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+              </ScrollView>
+            </View>
           </Tab>
-          <Tab heading={ <TabHeading><Text style={styles.textTab}>インフラ</Text></TabHeading>}>
-          <View style={styles.viewWrap}>
-          <ScrollView showsVerticalScrollIndicator={false}>
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          <EventComponent />
-          </ScrollView>
-          </View>
+          <Tab
+            heading={
+              <TabHeading>
+                <Text style={styles.textTab}>インフラ</Text>
+              </TabHeading>
+            }>
+            <View style={styles.viewWrap}>
+              <ScrollView showsVerticalScrollIndicator={false}>
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+                <EventComponent />
+              </ScrollView>
+            </View>
           </Tab>
         </Tabs>
-      <View style={styles.buttonWrap}>
-      <Button 
-        style={styles.registerButton} 
-        full={false} rounded danger
-        onPress={() => navigation.navigate('EventCreate')}
-        >
-        <Text style={styles.buttonText}> イベントを作成する</Text>
-      </Button>
-      </View>
+        <View style={styles.buttonWrap}>
+          <Button
+            style={styles.registerButton}
+            full={false}
+            rounded
+            danger
+            onPress={() => navigation.navigate('EventCreate')}>
+            <Text style={styles.buttonText}> イベントを作成する</Text>
+          </Button>
+        </View>
       </Container>
-      
-      </>
-    );
-  }
+    </>
+  );
+};
 
 const styles = StyleSheet.create({
   viewWrap: {
-    flex:1,
+    flex: 1,
     width: '100%',
-    alignItems: 'center'
+    alignItems: 'center',
   },
   textTab: {
     fontSize: 14,
     fontWeight: 'bold',
     textAlign: 'center',
     width: '100%',
-    
   },
-  registerButton:{
+  registerButton: {
     width: '70%',
-    height:100,
+    height: 100,
     marginBottom: 50,
     flex: 1,
     alignItems: 'center',
     borderRadius: 30,
-    backgroundColor: '#b6192e'
-    
-    
+    backgroundColor: '#b6192e',
   },
   buttonWrap: {
     width: '100%',
     alignItems: 'center',
     height: 100,
-    
   },
   buttonText: {
-    width:'100%',
-    textAlign: 'center'
-  }
-  
+    width: '100%',
+    textAlign: 'center',
+  },
 });
 
 export default EventFormScreen;
