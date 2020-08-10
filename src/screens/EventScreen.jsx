8e6298e2b7b21deab4a23b5f7506ff50a93926f1
Daichi0914 <goludrozya44@gmail.com>
2020-08-10 19:29:29 +0900

    EventScreenのコンフリクトの部分再修正

--- src/screens/EventScreen.jsx
+++ src/screens/EventScreen.jsx
@@ -20,84 +20,136 @@ import {
 } from '../utils/firebase.data';
 import { Button } from 'react-native';
 
-const EventScreen = ({navigation}) => {
-    
-    
-    return(
-        <ScrollView style={styles.viewWrap}>
-                <Text style={styles.mainText}>参加イベント</Text>
-                <View style={styles.border__1}></View>
-                <ScrollView 
-                    horizontal 
-                    showsHorizontalScrollIndicator={true}
-                >
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                </ScrollView>
 
 const EventScreen = ({ navigation }) => {
   const [joinEvents, setJoinEvents] = useState(null);
   const [hostEvents, setHostEvent] = useState(null);
   const [standbyEvents, setStandbyEvent] = useState(null);
 
-                <Text style={styles.mainText}>準備イベント</Text>
-                <View style={styles.border__3}></View>
-                <ScrollView
-                    horizontal 
-                    showsHorizontalScrollIndicator={true}
-                >
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                <EventComponent />
-                </ScrollView>
-        </ScrollView>
-    );
+  useEffect(() => {
+    firestore
+      .collection('version/1/users/MJpyUalJPCO2zIJ6J6fd3mOzwTo2/joinEvent')
+      .get()
+      .then((querySnapshot) => {
+        let joinEventsArray = [];
+        querySnapshot.forEach((doc) => {
+          joinEventsArray = [...joinEventsArray, doc.data()];
+        });
+        setJoinEvents(joinEventsArray);
+      });
+
+    firestore
+      .collection('version/1/users/MJpyUalJPCO2zIJ6J6fd3mOzwTo2/hostEvent')
+      .get()
+      .then((querySnapshot) => {
+        let hostEventArray = [];
+        querySnapshot.forEach((doc) => {
+          hostEventArray = [...hostEventArray, doc.data()];
+        });
+        setHostEvent(hostEventArray);
+      });
+
+    firestore
+      .collection('version/1/users/MJpyUalJPCO2zIJ6J6fd3mOzwTo2/standbyEvent')
+      .get()
+      .then((querySnapshot) => {
+        let standbyEventArray = [];
+        querySnapshot.forEach((doc) => {
+          standbyEventArray = [...standbyEventArray, doc.data()];
+        });
+        setStandbyEvent(standbyEventArray);
+      });
+  }, []);
+
+  return (
+    <ScrollView>
+      {/* <Button onPress={addDummyJoinedEvent} title="add dummy join data" /> */}
+      {/* <Button onPress={addDummyHostEvent} title="add dummy host data" /> */}
+      {/* <Button onPress={addDummystandbyEvent} title="add dummy standby data" /> */}
+      {/* <Button onPress={addDummyEvent} title="add dummy event data" /> */}
+      
+      <Text style={styles.mainText}>参加イベント</Text>
+      <View style={styles.border__1}></View>
+
+      <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+        {joinEvents && (
+          <FlatList
+          horizontal
+          data={joinEvents}
+          keyExtractor={(item) => item.id}
+          renderItem={(item) =>
+          <TouchableOpacity
+            onPress={() => navigation.navigate('EventFrame')}
+          >
+          <EventComponent eventData={item} />
+          </TouchableOpacity>}
+          />
+          )}
+      </ScrollView>
+
+      <Text style={styles.mainText}>主催イベント</Text>
+      <View style={styles.border__2}></View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+        {hostEvents && (
+          <FlatList
+            horizontal
+            data={hostEvents}
+            keyExtractor={(item) => item.id}
+            renderItem={(item) => 
+            <TouchableOpacity
+              onPress={() => navigation.navigate('EventFrame')}
+            ><EventComponent eventData={item} />
+            </TouchableOpacity>}
+          />
+        )}
+      </ScrollView>
+
+      <Text style={styles.mainText}>準備イベント</Text>
+      <View style={styles.border__3}></View>
+      <ScrollView horizontal showsHorizontalScrollIndicator={true}>
+        {standbyEvents && (
+          <FlatList
+            horizontal
+            data={standbyEvents}
+            keyExtractor={(item) => item.id}
+            renderItem={(item) => 
+            <TouchableOpacity
+              onPress={() => navigation.navigate('EventFrame')}
+            ><EventComponent eventData={item} />
+            </TouchableOpacity>}
+          />
+        )}
+      </ScrollView>
+    </ScrollView>
+  );
 };
 
 
 const styles = StyleSheet.create({
-    mainText: {
-        fontSize: 20,
-        fontWeight: 'bold',
-        padding: 5,
-        marginTop: 15
-        
-    },
-    border__1: {
-        borderBottomColor: '#A82402',
-        borderBottomWidth: 4,
-        marginBottom: 10,
-        width: '60%'
-        
-    },
-    border__2: {
-        borderBottomColor: '#ff9900',
-        borderBottomWidth: 4,
-        marginBottom: 10,
-        width: '60%'
-    
-    },
-    border__3: {
-        borderBottomColor: '#ffbbbb',
-        borderBottomWidth: 4,
-        marginBottom: 10,
-        width: '60%'
-    },
-    viewWrap: {
-        backgroundColor: 'white'
-    }
-    
+  mainText: {
+    fontSize: hp('2.5%'),
+    fontWeight: 'bold',
+    padding: 5,
+    marginTop: 5,
+  },
+  border__1: {
+    borderBottomColor: '#A82402',
+    borderBottomWidth: 4,
+    marginBottom: 25,
+    width: wp('50%'),
+  },
+  border__2: {
+    borderBottomColor: '#ff9900',
+    borderBottomWidth: 4,
+    marginBottom: 25,
+    width: wp('50%'),
+  },
+  border__3: {
+    borderBottomColor: '#ffbbbb',
+    borderBottomWidth: 4,
+    marginBottom: 25,
+    width: wp('50%'),
+  }
 });
 
 export default EventScreen;
