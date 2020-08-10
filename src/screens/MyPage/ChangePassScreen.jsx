976854cdda81647227843920a5c8c5ed90393183
Daichi0914 <goludrozya44@gmail.com>
2020-08-10 18:15:32 +0900

    ChangePassScreenを少し編集

--- src/screens/MyPage/ChangePassScreen.jsx
+++ src/screens/MyPage/ChangePassScreen.jsx
@@ -1,14 +1,51 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, StyleSheet, Text } from 'react-native';
+import {
+  Container,
+  Content,
+  List,
+  ListItem,
+  Input,
+  Icon,
+  Item,
+  Label,
+} from 'native-base';
 
 const PassSettingScreen = () => {
   return (
-    <View>
-      <Text>ChangePassScreen</Text>
-    </View>
+    <Container>
+      <Content>
+        <List>
+          <ListItem itemDivider />
+          {/* <ListItem style={styles.listItems}> */}
+            <Item>
+              <Input placeholder="Current Password" />
+            </Item>
+          {/* </ListItem> */}
+          <ListItem itemDivider />
+          <ListItem>
+            <Item>
+              <Input placeholder="New Password" />
+            </Item>
+          </ListItem>
+          <ListItem>
+            {/* ↓三項演算子で切り替え↓ */}
+            <Item error>
+              <Input placeholder="Again New Password" />
+              <Icon name="close-circle" />
+            </Item>
+          </ListItem>
+          <Label>パスワードが一致しません</Label>
+        </List>
+      </Content>
+    </Container>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  listItem: {
+    borderWidth: 0
+  },
+});
 
 export default PassSettingScreen;
