650539d9026f83ed560320f428b41aefbc662319
KogaYamada <koga0087y@gmail.com>
2020-08-11 16:55:03 +0900

    ちょっと修正

--- src/components-ad/atoms/auth-input.component.tsx
+++ src/components-ad/atoms/auth-input.component.tsx
@@ -4,41 +4,56 @@ import { TextInput } from 'react-native-gesture-handler';
 import { StyleSheet } from 'react-native';
 
 interface AuthInputProps {
-  placeholder: string;
-  errMesage: string;
+  placeholder?: string;
+  errMesage?: string;
+  secureTextEntry?: boolean;
 }
 
-const AuthInput: FC<AuthInputProps> = ({placeholder, errMesage = ''}) => {
+const AuthInput: FC<AuthInputProps> = ({
+  placeholder,
+  errMesage = '',
+  secureTextEntry,
+}) => {
   const [text, onChangeText] = useState('');
   return (
     <View>
       <TextInput
-        style={[styles.authInput, errMesage.trim() !== '' && styles.errorBorder]}
+        secureTextEntry={secureTextEntry}
+        style={[
+          styles.authInput,
+          errMesage.trim() !== '' && styles.errorBorder,
+        ]}
         onChangeText={(text) => onChangeText(text)}
         value={text}
         placeholder={placeholder}
       />
-      {errMesage.trim() !== '' && <Text style={styles.errText}>{errMesage}</Text>}
+      {errMesage.trim() !== '' && (
+        <Text style={styles.errText}>{errMesage}</Text>
+      )}
     </View>
   );
-}
+};
 
-export default AuthInput
+export default AuthInput;
 
 const styles = StyleSheet.create({
   authInput: {
     borderStyle: 'solid',
     borderWidth: 1,
     borderRadius: 7,
-    height: 40,
-    borderColor: '#707070',
-    backgroundColor: '#EBEBEB',
+    height: 48,
+    borderColor: '#DBDBDB',
+    backgroundColor: '#FBFBFB',
+    padding: 10,
+    fontSize: 18,
   },
   errorBorder: {
     borderColor: 'red',
   },
   errText: {
     color: 'red',
-    fontSize: 14
+    fontSize: 14,
+    paddingVertical: 7,
+    paddingHorizontal: 5,
   },
 });
