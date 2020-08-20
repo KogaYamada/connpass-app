21bd8532dc84dd942aa3d518833e816d8ff30aef
Nozomu Tsuruta <nozomu@NozomunoMacBook-Pro.local>
2020-08-20 23:20:54 +0900

    checkboxの追加

---
+++ src/components-ad/atoms/checkbox/checkbox.stories.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react-native';
+import { View } from 'native-base';
+
+/*************************[ props ]*****************************
+  checked: boolean;
+  setChecked: Dispatch<SetStateAction<boolean>>;      
+****************************************************************/
+
+export default storiesOf('Checkbox - atoms', module)
+  .addDecorator((story) => <View style={{ margin: 10 }}>{story()}</View>)
+  .add('checked', () => <Checkbox defaultValue="apple" />)
+  .add('unchecked', () => <Checkbox defaultValue="grape" />);
